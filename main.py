2dbd59866555c347a37c68ee35a92c98a8dda0bc
a96184324-web <a96184324@gmail.com>
2026-07-24 07:24:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def handle_image(event):
         )
         
         response = ai_client.models.generate_content(
-            model='gemini-2.0-flash-001',
+            model='gemini-2.5-pro',
             contents=[image, prompt]
         )
 
